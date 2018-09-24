228dd6b3a23d480c79934e1797caab92d54ec3e7
MehmetAygun <aygunme@itu.edu.tr>
2018-09-24 22:10:09 +0200

    resize and scale code

--- datasets/create_training_set.py
+++ datasets/create_training_set.py
@@ -1,6 +1,8 @@
 import numpy as np
 import h5py
 import scipy.io as sio
+import cv2
+
 
 def create_dataset(dataset):
 
@@ -21,8 +23,30 @@ def create_dataset(dataset):
 			for j,row in enumerate(label):
 				for k,value in enumerate(row):
 					labels[i][j][k] = mapping[value-1]
+		
+		print "Resizing and scaling images and labels..."
 
-		d = { "rgb_images":rgb_images, "depth_imahes":depth_images, "labels":labels}
+		rgbs=[]
+		depths=[]
+		masks=[]
+
+		max_depth,min_depth = np.max(depth_images),np.min(depth_images)
+
+		for rgb_image in rgb_images:
+			img = np.transpose(rgb_image,(1,2,0))
+			img = cv2.resize(img,(224,224),interpolation=cv2.INTER_LINEAR)
+			rgbs.append(rgbs)
+
+		for depth_image in depth_images:
+			depth_image = (depth_image - min_depth) / (1.*(max_depth-min_depth))*255
+			depth_image = cv2.resize(depth_image,(224,224),interpolation=cv2.INTER_NEAREST)
+			depths.append(depth_image.astype(int))
+
+		for label in labels:
+			label = cv2.resize(label,(224,224),interpolation=cv2.INTER_NEAREST)
+			masks.append(label)
+
+		d = { "rgb_images":rgb_images, "depth_images":depth_images, "masks":masks}
 
 		print "Saving images and labels..."
 
