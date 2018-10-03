5fdde1d1ef37f70cf64a884b2265952de51065f4
MehmetAygun <ayguenm@hpccremers5.informatik.tu-muenchen.de>
2018-10-03 14:06:09 +0200

    train/test split for data

--- data/nyu2_dataset.py
+++ data/nyu2_dataset.py
@@ -6,6 +6,7 @@ from data.base_dataset import BaseDataset
 from data.image_folder import make_dataset
 from PIL import Image
 import numpy as np 
+import scipy.io as sio
 
 class Nyu2Dataset(BaseDataset):
 	@staticmethod
@@ -17,9 +18,12 @@ class Nyu2Dataset(BaseDataset):
 		self.batch_size = opt.batch_size
 		self.root = opt.dataroot # path for nyu2.npy
 		self.nyu2 = np.load("{}/{}".format(self.root,"nyu2.npy")).tolist()
+		splits = sio.loadmat("{}/{}".format(self.root,"splits.mat"))
+		self.indexes = [x[0] for x in splits["trainNdxs"]] if opt.phase == "train" else [x[0] for x in splits["testNdxs"]]
 		assert(opt.resize_or_crop == 'resize_and_crop')
 
 	def __getitem__(self, index):
+		index = self.indexes[index]
 		rgb_image = np.array(self.nyu2["rgb_images"][index],dtype=np.uint8)
 		depth_image = self.nyu2["depth_images"][index]
 		depth_image = np.expand_dims(depth_image,axis=2)
@@ -42,7 +46,7 @@ class Nyu2Dataset(BaseDataset):
 		return {'rgb_image': rgb_image, 'depth_image': depth_image, 'mask': mask}
 
 	def __len__(self):
-		return len(self.nyu2["masks"])
+		return len(self.indexes)
 
 	def name(self):
 		return 'Nyu2Dataset'
--- datasets/download_nyu2.sh
+++ datasets/download_nyu2.sh
@@ -1,1 +1,2 @@
 wget http://horatio.cs.nyu.edu/mit/silberman/nyu_depth_v2/nyu_depth_v2_labeled.mat
+wget http://horatio.cs.nyu.edu/mit/silberman/indoor_seg_sup/splits.mat
